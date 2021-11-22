8eb3848a9beca3cfe09f21101e726d01cca8481a
Yannis <yannis.barba.dev@gmail.com>
2021-11-22 16:33:58 +0100

    reset all the files for the templates + let tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { Switch, Route } from 'react-router';
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import MyProjects from './pages/MyProjects';
-import AboutMe from './pages/AboutMe';
 
 function App() {
-  return (
-    <div id="App">
-      <header>
-        <Navbar />
-      </header>
-      <div id="main">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/my-projects" component={MyProjects} />
-          <Route path="/about-me" component={AboutMe} />
-        </Switch>
-      </div>
-
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
+  return <div id="App"></div>;
 }
 
 export default App;
